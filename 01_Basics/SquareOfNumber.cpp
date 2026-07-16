669cef4a8b8caea4325fb045ad815ae54f91d7bb
Trilok Aswal <trilokaswal050626@gmail.com>
2026-07-16 11:07:43 +0530

    new questions added

---
+++ 01_Basics/SquareOfNumber.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int Square = n * n;
+
+    cout << "Square: " << Square;
+
+    return 0;
+}
---
+++ 01_Basics/SumTwoNum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int a, b;
+    cout << "value of a: ";
+    cin>> a;
+    cout << "value of b: ";
+    cin >> b;
+    int Sum = a + b;
+    cout << "Sum: "<< Sum;
+    return 0;
+
+
+}
