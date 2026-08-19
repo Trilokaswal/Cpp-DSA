c4e9f2575d3e4cd1659bf2275ba438c8bf84b276
Trilok Aswal <trilokaswal260506@gmail.com>
2026-08-19 22:39:00 +0530

    new concepts jod diye

---
+++ 01_Basics/18_BoolDatatype.cpp
@@ -0,0 +1,18 @@
+// Bool data type is used to store boolean values, which can be either true or false.
+// it gives 1 for true and 0 for false.
+// it is used to find the state of a condition, whether it is true or false.
+
+#include <iostream>
+using namespace std;
+
+int main () { 
+
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+
+    cout << "Is coding fun? " << isCodingFun << endl; // Output: 1
+    cout << "Is fish tasty? " << isFishTasty << endl; // Output: 0
+
+    return 0;
+
+}
---
+++ 01_Basics/19_RelationalOperators.cpp
@@ -0,0 +1,28 @@
+// Relational operators are used to compare two values.
+// They return a boolean value (true or false) based on the comparison.
+
+// The following are the relational operators in C++:
+// 1. Equal to (==)
+// 2. Not equal to (!=)
+// 3. Greater than (>)
+// 4. Less than (<)
+// 5. Greater than or equal to (>=)
+// 6. Less than or equal to (<=)
+
+// These operators are commonly used in conditional statements
+// and loops to control the flow of the program.
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int a = 10;
+    int b = 20;
+
+    bool x = (a==b);
+    cout << x << endl;
+
+    return 0;
+    
+}
