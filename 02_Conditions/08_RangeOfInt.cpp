df3f6681ff93e7ea93dc149e13c4b1064c3db889
Trilok Aswal <trilokaswal260506@gmail.com>
2026-08-22 22:18:10 +0530

    Range of Int

---
+++ 02_Conditions/08_RangeOfInt.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int x = INT_MAX;
+    cout << x << endl; // output is 2147483647
+
+    int y = INT_MIN;
+    cout << y << endl; // output is -2147483648
+    return 0;
+    
+
+}
