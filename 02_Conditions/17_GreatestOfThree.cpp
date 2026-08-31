f2aa12e9622907632c7bee88f26f67eb1e5eb4aa
Trilok Aswal <trilokaswal260506@gmail.com>
2026-08-31 22:41:13 +0530

    If Else last part

--- 02_Conditions/17_GreatestOfThree.cpp
+++ 02_Conditions/17_GreatestOfThree.cpp
@@ -2,16 +2,32 @@
 using namespace std;
 int main () {
 
-    int a, b, c; 
-    cout << "Enter 3 numbers: " << endl;
-    cin>> a >> b >> c;
+   int a, b, c;
+   cout << " Enter first number : ";
+   cin>> a;
+
+   cout << " Enter second number : ";
+   cin >> b;
+
+   cout << " Enter third number : ";
+   cin >> c;
+
+   if (a, b, c > 0) // a, b, c to be positive integer
+   
+   { 
     
-    if (a > b && a > c) {
-        cout << "The greatest number is: " << a << endl;
-    } else if (b > a && b > c) {
-        cout << "The greatest number is: " << b << endl;
-    } else {
-        cout << "The greatest number is: " << c << endl;
-    }
-    return 0;
-}
+    if (a > b & a > c)
+    cout << a << " is the greatest integer ";
+    
+    else if ( b > c & b > a )
+    cout << b <<  " is the greatest integer ";
+
+    else cout << c <<  " is the greatest integer ";
+   
+   }
+   
+   else cout << " Please Enter a Positive integer "; 
+   
+   return 0;
+
+   } 
---
+++ 02_Conditions/20_VariableInsideIF.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+ 
+ 
+    int x = 0;
+    if (x) cout << "Anjali";
+    else cout << "Nitin"<< endl;
+ 
+    int a = 4 ;
+    if (a == 4)  cout << "Jitendra";
+    else cout << "Rohit";
+
+    return 0;
+}
